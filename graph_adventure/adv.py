a205d427a469fbdddd06a427a19e2d6a5a2a95fb
Angelo Deleon <rudy209@comcast.net>
2019-09-12 20:45:04 -0700

    All but last test works still working on it

--- graph_adventure/adv.py
+++ graph_adventure/adv.py
@@ -1,6 +1,8 @@
 from room import Room
 from player import Player
 from world import World
+from collections import deque
+
 
 import random
 
@@ -9,7 +11,8 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 
-# roomGraph={0: [(3, 5), {'n': 1}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
+# roomGraph = {0: [(3, 5), {'n': 1}], 1: [
+#     (3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}]}
 # roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}]}
 roomGraph={0: [(3, 5), {'n': 1, 's': 5, 'e': 3, 'w': 7}], 1: [(3, 6), {'s': 0, 'n': 2, 'e': 12, 'w': 15}], 2: [(3, 7), {'s': 1}], 3: [(4, 5), {'w': 0, 'e': 4}], 4: [(5, 5), {'w': 3}], 5: [(3, 4), {'n': 0, 's': 6}], 6: [(3, 3), {'n': 5, 'w': 11}], 7: [(2, 5), {'w': 8, 'e': 0}], 8: [(1, 5), {'e': 7}], 9: [(1, 4), {'n': 8, 's': 10}], 10: [(1, 3), {'n': 9, 'e': 11}], 11: [(2, 3), {'w': 10, 'e': 6}], 12: [(4, 6), {'w': 1, 'e': 13}], 13: [(5, 6), {'w': 12, 'n': 14}], 14: [(5, 7), {'s': 13}], 15: [(2, 6), {'e': 1, 'w': 16}], 16: [(1, 6), {'n': 17, 'e': 15}], 17: [(1, 7), {'s': 16}]}
@@ -21,8 +24,35 @@ player = Player("Name", world.startingRoom)
 
 
 # FILL THIS IN
-traversalPath = ['n', 's', 'e', 'w']
+traversalPath = ['n', 's']
 graph = {}
+
+
+def BFS(start, end):
+        d = deque()
+        visited = []
+        d.append(start)
+        if end in graph[start]:
+            visited.append(start)
+            visited.append(end)
+            return visited
+        while d:
+            vertex = d.popleft()
+            if "?" in list(graph[player.currentRoom.id].values()):
+                visited.append(vertex)
+                return visited
+            if vertex == end:
+                visited.append(vertex)
+                return visited
+            if end in visited:
+                return visited
+            if vertex not in visited:
+                visited.append(vertex)
+                next_nodes = graph[vertex]
+                for k, v in next_nodes.items():
+                    print("key", k, "value", v)
+                    d.append(v)
+
 
 def populateGraph(roomId):
     exits = player.currentRoom.getExits()
@@ -33,10 +63,11 @@ def populateGraph(roomId):
         for e in exits:
             graph[player.currentRoom.id].update({e: "?"})
 
-    # roomId = player.currentRoom.id 
+    # roomId = player.currentRoom.id
     # graph[roomId] = {}
     # for e in exits:
     #     graph[roomId].update({e: "?"})
+
 
 def updateGraph(prevRoom, newRoom, direction):
     graph[prevRoom].update({direction: newRoom})
@@ -50,8 +81,6 @@ def updateGraph(prevRoom, newRoom, direction):
         graph[newRoom].update({"e": prevRoom})
 
 
-
-
 def playerTravel():
     visited_rooms = set()
     player.currentRoom = world.startingRoom
@@ -63,61 +92,39 @@ def playerTravel():
         populateGraph(player.currentRoom.id)
 
         for e in graph[player.currentRoom.id]:
-            print(graph[player.currentRoom.id], e)
             if graph[player.currentRoom.id][e] == "?":
+                print(player.currentRoom.id, graph[player.currentRoom.id], e)
                 move = e
                 traversalPath.append(move)
                 player.travel(move)
                 visited_rooms.add(player.currentRoom.id)
+                populateGraph(player.currentRoom)
+                updateGraph(lastRoom.id, player.currentRoom.id, move)
+                lastRoom = player.currentRoom
                 break
-            while "?" not in list(graph[player.currentRoom.id].values()):
-                reversedPath = []
-                for i in traversalPath:
-                    reversedPath.append(i)
-                reversedPath.reverse()
-                for idx, d in enumerate(reversedPath):
-                    if d == "n":
-                        reversedPath[idx] = "s"
-                        continue
-                    if d == "s":
-                        reversedPath[idx] = "n"
-                        continue
-                    if d == "e":
-                        reversedPath[idx] = "w"
-                        continue
-                    if d == "w":
-                        reversedPath[idx] = "e"
-                        continue
-                for idx, i in enumerate(reversedPath):
-                    player.travel(i)
-                    if "?" in list(graph[player.currentRoom.id].values()):
-                        if idx == 0:
-                            traversalPath += reversedPath[0]
-                        else:
-                            traversalPath += (reversedPath[0:idx+1])
-                        break
-
-        visited_rooms.add(player.currentRoom.id)
-        populateGraph(player.currentRoom)
-        updateGraph(lastRoom.id, player.currentRoom.id, move)
-        lastRoom = player.currentRoom
-        print(len(graph))
-    # roomId = player.currentRoom.id
-    # populateGraph()
-    # for e in graph[roomId]:
-    #     if graph[roomId][e] == "?":
-    #         move = e
-    #         break
-    # player.travel(move)
-    # populateGraph()
-    # print(roomId)
+            if "?" not in list(graph[player.currentRoom.id].values()):
+                for k, v in graph.items():
+                    if "?" in v.values():
+                        target = k
+                        print(player.currentRoom.id, target)
+                        print(len(visited_rooms))
+                        path = BFS(player.currentRoom.id, target)
+                        print(len(visited_rooms))
+                        print(path)
+                        for i in path:
+                            for k, v in graph[player.currentRoom.id].items():
+                                if v == i:
+                                    traversalPath.append(k)
+                                    player.travel(k)
+                                    visited_rooms.add(player.currentRoom.id)
+                                    populateGraph(player.currentRoom)
+                                    updateGraph(lastRoom.id,
+                                                player.currentRoom.id, k)
+                                    lastRoom = player.currentRoom
+                                    if "?" in list(graph[player.currentRoom.id].values()):
+                                        break
 
 playerTravel()
-
-
-
-
-
 
 
 # TRAVERSAL TEST
@@ -133,7 +140,6 @@ if len(visited_rooms) == len(roomGraph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(roomGraph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
